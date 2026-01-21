f835a03e438daade0613442291d23ce960445111
Nuthan12 <96606507+Nuthan12@users.noreply.github.com>
2026-01-21 22:31:44 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const habits = JSON.parse(localStorage.getItem("habits")) || [];
+
+function render() {
+  const list = document.getElementById("habitList");
+  list.innerHTML = "";
+  habits.forEach((h, i) => {
+    list.innerHTML += `
+      <li>
+        <input type="checkbox" ${h.done ? "checked" : ""} 
+               onchange="toggle(${i})">
+        ${h.name}
+      </li>`;
+  });
+  localStorage.setItem("habits", JSON.stringify(habits));
+}
+
+function addHabit() {
+  const input = document.getElementById("habitInput");
+  habits.push({ name: input.value, done: false });
+  input.value = "";
+  render();
+}
+
+function toggle(i) {
+  habits[i].done = !habits[i].done;
+  render();
+}
+
+render();
